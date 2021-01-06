d73b6d46bc22fce15a4db0f4aa98e4a36ddebe99
SeongYeolWoo <tjddufdn2@naver.com>
2021-01-06 12:12:44 +0900

    Chapter3

---
+++ Chapter3/src/Operator/OperatorEx5.java
@@ -0,0 +1,18 @@
+package Operator;
+
+public class OperatorEx5 {
+
+	public static void main(String[] args) {
+	
+		int num1 = 0B00001010; //10
+		int num2 = 0B00000101; //5
+		
+		System.out.println(num1 & num2);
+		System.out.println(num1 | num2);
+		System.out.println(num1 ^ num2);
+		
+		System.out.println(num2 << 3); // num2 * 2^3
+		System.out.println(num2 >> 1); // num2 / 2^1
+		
+		}
+}
